2a1cf36411977c3170c4a802c7ed53ed12022538
José Daniel Grijalba O <jose.jdgo97@gmail.com>
2023-09-01 18:39:17 -0500

    primer cambio

---
+++ lib/ui/pages/home/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _initialCameraPosition = const CameraPosition(
+    target: LatLng(0, 0),
+  );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: GoogleMap(
+        initialCameraPosition: _initialCameraPosition,
+      ),
+    );
+  }
+}
